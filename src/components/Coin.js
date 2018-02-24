7e9990ac1a5f65b636d53898715796ba45b0e6e1
Rodrigo Ariza <rodri.one.2@gmail.com>
2018-02-24 21:37:57 +0100

    Refactor and socket added

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,39 +1,37 @@
 import React from 'react';
 
 const coinDetailStyle = {
-    display: 'block',
-    color: 'white'
+	display: 'block',
+	color: 'white'
 }
 const diffOneDayStyle = {
-    display: 'block',
-    color: 'green'
+	display: 'block',
+	color: 'green'
 }
 export default class Coin extends React.Component {
-    render() {
-        if (this.props.diffOneDay < 0) {
-            diffOneDayStyle.color = 'red';
-        }
-        return (
-            <div className="container">
-                <div className="coinContainer">
-                    <div className="row">
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <h1 className="centered" id="coinName">{this.props.name}</h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12 col-md-12 col-lg-12">
-                            <div className="centered">
-                                <span style={coinDetailStyle}>{this.props.fullname}</span>
-                                <span style={coinDetailStyle}>{this.props.usdPrice} $</span>
-                                <span style={coinDetailStyle}>{this.props.eurPrice} €</span>
-                                <br/>
-                                <span style={diffOneDayStyle}>{this.props.diffOneDay} %</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		if (this.props.diffOneDay < 0) {
+			diffOneDayStyle.color = 'red';
+		}
+		return (
+			<div className="coinContainer">
+				<div className="row">
+					<div className="col-sm-12 col-md-12 col-lg-12">
+						<h1 className="centered" id="coinName">{this.props.name}</h1>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-12 col-md-12 col-lg-12">
+						<div className="centered">
+							<span style={coinDetailStyle}>{this.props.fullname}</span>
+							<span style={coinDetailStyle}>{this.props.usdPrice} $</span>
+							<span style={coinDetailStyle}>{this.props.eurPrice} €</span>
+							<br />
+							<span style={diffOneDayStyle}>{this.props.diffOneDay} %</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 } 
--- src/components/MainCrypto.js
+++ src/components/MainCrypto.js
@@ -2,139 +2,134 @@ import React from 'react';
 import axios from 'axios';
 
 import Coin from './Coin.js';
-// It simply creates an object to insert in reactive vars
-const buildCoinInfo = function (name, fullname, usdPrice, eurPrice, diffOneDay) {
-	return { name, fullname, usdPrice, eurPrice, diffOneDay };
-}
-
 export default class MainCrypto extends React.Component {
-    constructor () {
-        super();
+	constructor() {
+		super();
+		this.constants = {
+			COINBASE_URL: 'https://www.coincap.io/page/',
+			BTC: 'BTC',
+			ETH: 'ETH',
+			XRP: 'XRP'
+		};
+		
+		this.state = {
+			coins: {
+				BTC: {
+					loaded: false,
+					fullname: '',
+					usdPrice: '',
+					eurPrice: '',
+					diffOneDay: ''
+				},
+				ETH: {
+					loaded: false,
+					fullname: '',
+					usdPrice: '',
+					eurPrice: '',
+					diffOneDay: ''
+				},
+				XRP: {
+					loaded: false,
+					fullname: '',
+					usdPrice: '',
+					eurPrice: '',
+					diffOneDay: ''
+				}
+			},
+			showDetail: false
+		}
 
-        this.state = {
-            coins: [{
-                name: '',
-                fullname: '',
-                usdPrice: '',
-                eurPrice: '',
-                diffOneDay: '',
-                url: 'BTC',
-            }, {
-                name: '',
-                fullname: '',
-                usdPrice: '',
-                eurPrice: '',
-                diffOneDay: '',
-            }, {
-                name: '',
-                fullname: '',
-                usdPrice: '',
-                eurPrice: '',
-                diffOneDay: '',
-            }],
-            allLoaded: false,
-            showDetail: false,
-        }
+		this.coinDetail.bind(this);
+		this.startSocket.bind(this);
+		this.initCoins.bind(this);
+		this.allLoaded.bind(this);
+	}
 
-        this.coinDetail.bind(this);
-    }
+	startSocket () {
+		const instance = this;
+		const io = require('socket.io-client');
+		// Socket is over HTTP protocol, will not work with https deployment
+		const socket = io.connect('http://socket.coincap.io');
+		socket.on('trades', (data) => {
+			const coinName = data.message.coin;
+			const msg = data.message.msg;
 
-    componentDidMount() {
-        // const instance = this;
+			const coins = this.state.coins;
+			if (coins[coinName]) {
+				const coinInfo = {
+					usdPrice: msg.price,
+					diffOneDay: msg.cap24hrChange
+				}
+				Object.assign(coins[coinName], coinInfo);
+				instance.setState({ coins });
+			}
+		});
+	}
 
-        // const io = require('socket.io-client');
-        // Socket is over HTTP protocol, will not work with https deployment
-        // const socket = io.connect('http://socket.coincap.io');
+	// Sets the init values for the 3 coins
+	initCoins () {
+		const coinBaseUrl = this.constants.COINBASE_URL;
+		function setCoin (response) {
+			const coins = this.state.coins;
+			const coinInfo = {
+				name: response.data.id, 
+				fullname: response.data.display_name, 
+				usdPrice: response.data.price_usd,
+				eurPrice: response.data.price_eur, 
+				diffOneDay: response.data.cap24hrChange,
+				loaded: true
+			};
+			coins[response.data.id] = coinInfo;
+			this.setState({ coins });
+		}
+		axios.get(coinBaseUrl.concat('BTC')).then(setCoin.bind(this));
+		axios.get(coinBaseUrl.concat('ETH')).then(setCoin.bind(this));
+		axios.get(coinBaseUrl.concat('XRP')).then(setCoin.bind(this));
+	}
 
-        // socket.on('trades', (data) => {
-        //     const coinName = data.message.coin;
-        //     const msg = data.message.msg;
-        //     // Differ from BTC,ETH and XRP
-        //     switch (coinName) {
-        //         case 'BTC':
-        //             instance.setState({ BTC: buildCoinInfo(msg.short, msg.long, msg.price, msg.time, '') });
-        //             break;
-        //         case 'ETH': 
-        //             instance.setState({ ETH: buildCoinInfo(msg.short, msg.long, msg.price, msg.time, '') });
-        //             break;
-        //         case 'XRP':
-        //             instance.setState({ XRP: buildCoinInfo(msg.short, msg.long, msg.price, msg.time, '') });
-        //             break;
-        //         default:
-        //             console.log('Ninguno')
-        //     }
-        // });
-        
-    }
+	allLoaded () {
+		const coins = this.state.coins;
+		return coins.BTC.loaded && coins.ETH.loaded && coins.XRP.loaded;
+	}
 
-    componentWillMount() {
-        const instance = this;
-        const coinsLoad = [false, false, false];
-        function allLoaded() {
-            let response = 0;
-            coinsLoad.forEach(function (e) {
-                if (e) response++;
-            });
-            return response === 3;
-        }
-        // BTC call
-        axios.get('https://www.coincap.io/page/BTC').then(function (response) {
-            const coins = instance.state.coins;
-            const coinInfo = buildCoinInfo(response.data.id, response.data.display_name, response.data.price_usd, response.data.price_eur, response.data.cap24hrChange);
-            coins[0] = coinInfo;
-            console.log(response)
-            instance.setState({ coins });
-            coinsLoad[0] = true;
-            if (allLoaded()) instance.setState({ allLoaded: true });
-        });
-        // ETH Call
-        axios.get('https://www.coincap.io/page/ETH').then(function (response) {
-            const coins = instance.state.coins;
-            const coinInfo = buildCoinInfo(response.data.id, response.data.display_name, response.data.price_usd, response.data.price_eur, response.data.cap24hrChange);
-            coins[1] = coinInfo;
-            instance.setState({ coins });
-            coinsLoad[1] = true;
-            if (allLoaded()) instance.setState({ allLoaded: true });
-        });
-        // XRP Call
-        axios.get('https://www.coincap.io/page/XRP').then(function (response) {
-            const coins = instance.state.coins;
-            const coinInfo = buildCoinInfo(response.data.id, response.data.display_name, response.data.price_usd, response.data.price_eur, response.data.cap24hrChange);
-            coins[2] = coinInfo;
-            instance.setState({ coins });
-            coinsLoad[2] = true;
-            if (allLoaded()) instance.setState({ allLoaded: true });
-        });
-    }
+	componentDidMount() {
+		this.startSocket();
+	}
 
-    coinDetail (event) {
-        console.log('Detail...');
-    }
+	componentWillMount() {
+		this.initCoins();
+	}
 
-    render () {
-        if (this.state.allLoaded && !this.state.showDetail) {
-            const instance = this;
-            const allCoins = this.state.coins.map(function (coinInfo, index) {
-                const href = `/coins/${coinInfo.name}`;
-                console.log(coinInfo)
-                return <a href={href} key={index} onClick={instance.coinDetail}>
-                    <Coin name={coinInfo.name}
-                        fullname={coinInfo.fullname}
-                        usdPrice={coinInfo.usdPrice}
-                        eurPrice={coinInfo.eurPrice} 
-                        coinDetail={instance.coinDetail}
-                        diffOneDay={coinInfo.diffOneDay}
-                        key={index} /></a>
-            });
-            return (
-                <div className="aligner">
-                    {allCoins}
-                </div>
-            )
-        } else if (this.state.allLoaded && this.state.showDetail) {
-            return (<h5>Detalle...</h5>)
-        } else {
-            return (<h5>Loading...</h5>)
-        }
-    }
+	coinDetail(event) {
+		console.log('Detail...');
+	}
+
+	render() {
+		if (this.allLoaded() && !this.state.showDetail) {
+			const stateCoins = this.state.coins;
+			const allCoins = Object.keys(stateCoins).map(function (coinName, index) {
+				const thisCoinInfo = stateCoins[coinName];
+				const href = `/coins/${thisCoinInfo.name}`;
+				return <a className="col-sm-4" key={index} href={'#'} /*onClick={instance.coinDetail}*/>
+					<Coin name={thisCoinInfo.name}
+						fullname={thisCoinInfo.fullname}
+						usdPrice={thisCoinInfo.usdPrice}
+						eurPrice={thisCoinInfo.eurPrice}
+						coinDetail={false}
+						diffOneDay={thisCoinInfo.diffOneDay}
+						key={index} /></a>
+			});
+			return (
+				<div className="aligner">
+					<div className="row mainRow">
+						{allCoins}
+					</div>
+				</div>
+			)
+		} else if (this.allLoaded() && this.state.showDetail) {
+			return (<h5>Detalle...</h5>)
+		} else {
+			return (<h5>Loading...</h5>)
+		}
+	}
 }
